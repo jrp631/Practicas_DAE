398306d8f0be7aa90f510c5c842108ba95674dfe
Juan Romon Peña <jrp631@alumnos.unican.es>
2025-05-03 13:01:33 +0200

    REST controller implemented

--- polaflix_juan_romon/src/main/java/es/unican/juan/romon/polaflix_juan_romon/Controlador/UsuarioController.java
+++ polaflix_juan_romon/src/main/java/es/unican/juan/romon/polaflix_juan_romon/Controlador/UsuarioController.java
@@ -19,8 +19,12 @@ import es.unican.juan.romon.polaflix_juan_romon.Repositorios.SerieRepositorio;
 import es.unican.juan.romon.polaflix_juan_romon.Repositorios.UsuarioRepositorio;
 import es.unican.juan.romon.polaflix_juan_romon.Servicio.SerieService;
 import es.unican.juan.romon.polaflix_juan_romon.Servicio.UsuarioService;
+import es.unican.juan.romon.polaflix_juan_romon.Servicio.UsuarioService.CapituloNoEncontradoException;
+import es.unican.juan.romon.polaflix_juan_romon.Servicio.UsuarioService.SerieNoEncontradaException;
+import es.unican.juan.romon.polaflix_juan_romon.Servicio.UsuarioService.UsuarioNoEncontradoException;
 import es.unican.juan.romon.polaflix_juan_romon.Vistas.Vistas;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -48,17 +52,10 @@ public class UsuarioController {
     @GetMapping(value = "/{id}/series-empezadas")
     @JsonView(Vistas.SeriesEmpezadas.class)
     public ResponseEntity<List<Serie>> getSeriesEmpezadas(@PathVariable("id") String userId) {
-        // Optional<Usuario> usuario = usuarioRepositorio.findById(Integer.parseInt(userId));
         ResponseEntity<List<Serie>> result;
-
-        // if (usuario.isPresent()) {
-        //     result = ResponseEntity.ok(usuario.get().getSeriesTerminadas());
-        // } else {
-        //     result = ResponseEntity.notFound().build();
-        // }
         List<Serie> seriesEmpezadas = usuarioService.getSeriesEmpezadas(userId);
 
-        if (seriesEmpezadas.size() > 0) {
+        if (seriesEmpezadas.size() >= 0) {
             result = ResponseEntity.ok(seriesEmpezadas);
         } else {
             result = ResponseEntity.notFound().build();
@@ -70,15 +67,10 @@ public class UsuarioController {
     @GetMapping(value = "/{id}/series-terminadas")
     @JsonView(Vistas.SeriesTerminadas.class)
     public ResponseEntity<List<Serie>> getSeriesTerminadas(@PathVariable("id") String userId) {
-        // Optional<Usuario> usuario = usuarioRepositorio.findById(Integer.parseInt(userId));
         ResponseEntity<List<Serie>> result;
         List<Serie> seriesTerminadas = usuarioService.getSeriesTerminadas(userId);
-        // if (usuario.isPresent()) {
-        //     result = ResponseEntity.ok(usuario.get().getSeriesTerminadas());
-        // } else {
-        //     result = ResponseEntity.notFound().build();
-        // }
-        if (seriesTerminadas.size() > 0) {
+        
+        if (seriesTerminadas.size() >= 0) {
             result = ResponseEntity.ok(seriesTerminadas);
         } else {
             result = ResponseEntity.notFound().build();
@@ -90,120 +82,85 @@ public class UsuarioController {
     @GetMapping(value = "/{id}/series-pendientes")
     @JsonView(Vistas.SeriesPendientes.class)
     public ResponseEntity<List<Serie>> getSeriesPendientes(@PathVariable("id") String userId) {
-        // Optional<Usuario> usuario = usuarioRepositorio.findById(Integer.parseInt(userId));
         ResponseEntity<List<Serie>> result;
         List<Serie> seriesPendientes = usuarioService.getSeriesPendientes(userId);
-        // if (usuario.isPresent()) {
-        //     result = ResponseEntity.ok(usuario.get().getSeriesPendientes());
-        // } else {
-        //     result = ResponseEntity.notFound().build();
-        // }
 
-        if (seriesPendientes.size() > 0) {
+        if (seriesPendientes.size() >= 0) {
             result = ResponseEntity.ok(seriesPendientes);
         } else {
             result = ResponseEntity.notFound().build();
         }
 
         return result;
-        // return usuario.map(Usuario::getSeriesPendientes)
-                    //   .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
     }
 
-    @GetMapping(value = "/{id}/cargos")
+    @GetMapping(value = "/{id}/cargos")//FIXME: no llamar a usuarioRepositorio y pasar directamente el id
     @JsonView(Vistas.CargosUsuario.class)
     public ResponseEntity<List<Cargo>> getAllCargos(@PathVariable ("id") String id, @RequestParam(required = false) LocalDate fecha_ini, @RequestParam(required = false) LocalDate fecha_fin) {
         // Optional<Usuario> usuario = usuarioRepositorio.findById(Integer.parseInt(id));
         ResponseEntity<List<Cargo>> result;
         List<Cargo> cargos = usuarioService.getCargos(usuarioRepositorio.findById(Integer.parseInt(id)).get(), fecha_ini, fecha_fin);
 
-        // if (usuario.isPresent()) {
-        //     List<Cargo> cargos = usuario.get().getCargosUsuario();
-
-        //     if (fecha_ini == null &&  fecha_fin == null){
-        //         result = ResponseEntity.ok(cargos);
-        //     } else { // REFACTORIZAR
-        //         for (Cargo c : cargos) { //retornamos los cargos que se encuentran entre las fechas
-        //             if (fecha_ini != null && fecha_fin == null) { //cargos despues de una fecha
-        //                 if(c.getFecha().isBefore(fecha_ini)) {
-        //                     cargos.remove(c);
-        //                 }
-        //             } else if (fecha_ini == null && fecha_fin != null) { // cargos antes de una fecha
-        //                 if (c.getFecha().isAfter(fecha_fin)) {
-        //                     cargos.remove(c);
-        //                 }
-        //             } else { //cargos entre dos fechas
-        //                 if (c.getFecha().isBefore(fecha_ini) || c.getFecha().isAfter(fecha_fin)) {
-        //                     cargos.remove(c);
-        //                 }
-        //             } 
-                        
-        //         }
-        //         result = ResponseEntity.ok(cargos);
-        //     }
-        // } else {
-        //     result = ResponseEntity.notFound().build();
-        // }
-
-        if(cargos.size() > 0) {
+        if(cargos.size() >= 0) {
             result = ResponseEntity.ok(cargos);
         } else {
             result = ResponseEntity.notFound().build();
         }
 
         return result;
-        // return usuario.map(Usuario::getCargosUsuario)
-                    //   .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
     }
     
-    @PutMapping(value = "/{id}/series-empezadas") // TODO: check implementation
-    public ResponseEntity putVerCapitulo(@PathVariable("id") String userId,@RequestBody String serieId ,@RequestBody String capituloId) {
-        Optional<Usuario> usuario = usuarioRepositorio.findById(Integer.parseInt(userId));
-        Optional<Serie> serie = serieRepositorio.findById(Integer.parseInt(serieId));
+    // FIXME 
+    @PutMapping(value = "/{id}/series-empezadas/{serieId}/{capituloId}") // TODO: check implementation
+    public ResponseEntity<String> putVerCapitulo(@PathVariable("id") String userId,@PathVariable("serieId") String idSerie ,@PathVariable ("capituloId") String idCapitulo) {
+        ResponseEntity<String> result;
 
-        ResponseEntity result;
-
-        if (usuario.isPresent() && serie.isPresent()) {
-            Capitulo capitulo = serie.get().getCapituloFromSerie(Integer.parseInt(capituloId));
-            usuario.get().verCapitulo(capitulo);
-            usuarioRepositorio.save(usuario.get());
-            result = ResponseEntity.ok().build();
-        } else {
-            result = ResponseEntity.notFound().build();
+        try {
+            usuarioService.putVerCapitulo(userId, idSerie, idCapitulo);
+            result = ResponseEntity.ok("Capitulo visto");
+        } catch (UsuarioNoEncontradoException e) {
+            result = ResponseEntity.notFound().build(); // 404 user not found
+        } catch(SerieNoEncontradaException e) { 
+            result = ResponseEntity.notFound().build(); // 404 serie not found
+        } catch(CapituloNoEncontradoException e) { 
+            result = ResponseEntity.notFound().build(); // 404 capitulo not found
         }
+
         return result;
     }
 
-    @PutMapping(value = "{id}/series-pendientes")
-    public ResponseEntity putAgregarSeriePendiente(@PathVariable("id") String userId, @RequestBody String serieId) {
-        Optional<Usuario> usuario = usuarioRepositorio.findById(Integer.parseInt(userId));
-        Optional<Serie> serie = serieRepositorio.findById(Integer.parseInt(serieId));
+    @PutMapping(value = "{id}/series-pendientes/{serieId}")
+    public ResponseEntity<String> putAgregarSeriePendiente(@PathVariable("id") String userId, @PathVariable String serieId) {
+        ResponseEntity<String> result;
 
-        ResponseEntity result;
-
-        if (usuario.isPresent() && serie.isPresent()) {
-            usuario.get().agregarSeriePendiente(serie.get());
-            usuarioRepositorio.save(usuario.get());
-            result = ResponseEntity.ok().build();
-        } else {
+        try {
+            usuarioService.putSeriePendiente(userId, serieId);
+            result = ResponseEntity.ok("Serie añadida a pendientes");
+        } catch (UsuarioNoEncontradoException e1) {
+            result = ResponseEntity.notFound().build(); // 404 user not found
+        } catch (SerieNoEncontradaException e) { // 404 serie not found
             result = ResponseEntity.notFound().build();
         }
+
         return result;
     }
 
-
-    // TODO: delete : eliminar serie de pendientes
-
-    // @PostMapping(value = "/{id}/series-empezadas")      
-    // public String postVerCapitulo(@RequestBody Integer idCapitulo, @RequestBody Integer idSerie) {
-    //     //TODO: process POST request
-        
-    //     // ver un capitulo y save 
-
-
-    //     return ;
-    // }
     
+    @DeleteMapping(value = "/{id}/series-pendientes/{idSerie}")
+    public ResponseEntity<String> deleteSeriePendiente(@PathVariable("id") String userId, @PathVariable("idSerie") String serieId) {
+        ResponseEntity<String> result;
+        // TODO 
+        try {
+            usuarioService.deleteSeriePendiente(userId, serieId);
+            result = ResponseEntity.ok("Serie eliminada de pendientes");
+        } catch (UsuarioNoEncontradoException e1) {
+            result = ResponseEntity.notFound().build(); // 404 user not found
+        } catch (SerieNoEncontradaException e) { // 404 serie not found
+            result = ResponseEntity.notFound().build();
+        }
+
+        return result;
+    }
 
 }
 
